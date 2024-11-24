e757aaf7b9dcc559c086d007edf55ca04b344a9f
Mauro Alves <mauro.alves.aguiar@outlook.com>
2024-11-24 19:10:26 -0300

    Auto-generated commit

--- server/angular-app-manifest.mjs
+++ server/angular-app-manifest.mjs
@@ -4,8 +4,8 @@ export default {
   inlineCriticalCss: true,
   routes: undefined,
   assets: new Map([
-['index.csr.html', {size: 4196, hash: 'a6d45c85706a0673facbb6afa06de7bdded2d98d57bc4d97f13b47de73557c6b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
-['index.server.html', {size: 1973, hash: 'f3fb382d66631108a33a8f8a476567d421704ad012c5fcd2c5e2088636c9780d', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
+['index.csr.html', {size: 4196, hash: '5b4c575bb35fd91222f693dac8af8da963b8781cd25f36018496d80cb10ff3d7', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)}], 
+['index.server.html', {size: 1973, hash: 'f399f4293e16510221a24a891280a0f7a55f7eea8829cdd883b2c704265c9322', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)}], 
 ['styles-JUGAZ4YJ.css', {size: 267531, hash: 'm/SfHTKgVic', text: () => import('./assets-chunks/styles-JUGAZ4YJ_css.mjs').then(m => m.default)}]
 ]),
   locale: undefined,
